200bfd0fa1b3fb1ca09e8759939b4cc347fd14c9
Stefano <stefano@mbp.local>
2025-09-01 09:14:07 +0200

    fix: Node 24 JSON import compatibility; bump to 0.12.2

--- utils/mqtt-hadiscovery.js
+++ utils/mqtt-hadiscovery.js
@@ -2,7 +2,13 @@
 
 import { MeianLogger } from 'ialarm'
 import { configHandler }  from './config-handler.js'
-import pjson from '../package.json' assert { type: 'json' }
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath, pathToFileURL } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const pkgPath = path.resolve(__dirname, '../package.json')
+const pjson = JSON.parse(fs.readFileSync(pkgPath, 'utf8'))
 
 export default function (config, zonesToConfig, reset, deviceInfo) {
   const logger = MeianLogger(config.verbose ? 'debug' : 'info')
